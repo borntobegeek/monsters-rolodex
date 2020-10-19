c6012470d7a92321bb97cef80c6d42c2b62ed565
Adam Balogh <91adambalogh@gmail.com>
2020-10-19 14:39:29 +0200

    Updates

---
+++ precache-manifest.283fa6c27d93d3d4f624a341ff94fde4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "419f8a70ee39b5b92084d71905c99fbd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b0449ba87f96bc4ae626",
+    "url": "/monsters-rolodex/static/css/main.e483390e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0449ba87f96bc4ae626",
+    "url": "/monsters-rolodex/static/js/main.ae0dbde5.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
